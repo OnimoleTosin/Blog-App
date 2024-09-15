9d3128caaaa12f6eff9deb11d092472b54ff2494
Onimole Tosin <onimolesodiq282@gmail.com>
2024-09-15 04:39:50 +0100

    components and header

---
+++ src/app/components/cardList/CardList.jsx
@@ -0,0 +1,13 @@
+import Pagination from '@/app/components/pagination/Pagination'
+import styles from './cardList.module.css'
+
+const CardList = () => {
+  return (
+    <div className={styles.container} >
+      CardList
+     <Pagination/>
+    </div>
+  )
+}
+
+export default CardList
